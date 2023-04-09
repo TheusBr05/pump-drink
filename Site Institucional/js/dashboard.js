cb5dcc364ea960f57a1747446691dd2a4c6e8adc
Melissa Neves <melissa.neves@sptech.school>
2023-04-08 23:12:52 -0300

    @melissaSBneves Header dashboard

---
+++ Site Institucional/js/dashboard.js
@@ -0,0 +1,66 @@
+
+  const graficoBarra_hoje = document.getElementById('desempenho_hoje');
+  const graficoPizza_regiao = document.getElementById('desempenho_regiao');
+  const graficoLinha_semana = document.getElementById('desempenho_semana');
+
+
+  new Chart(graficoBarra_hoje, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  new Chart(graficoBarra_hoje, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  new Chart(graficoBarra_hoje, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+
+  
+
