f3b01f715249ed5d3370233a2b432725b9a85f39
Melissa Neves <melissa.neves@sptech.school>
2023-05-19 20:50:41 -0300

    @melissaSBneves Conexão do banco de dados com o gráfico de desempenho

--- Site Institucional/src/controllers/medidaController.js
+++ Site Institucional/src/controllers/medidaController.js
@@ -4,11 +4,11 @@ function buscarUltimasMedidas(req, res) {
 
     const limite_linhas = 7;
 
-    var idAquario = req.params.idAquario;
+    var idBebida = req.params.idBebida
 
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+    medidaModel.buscarUltimasMedidas(idBebida, limite_linhas).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -58,8 +58,25 @@ function totalSaidas(req, res){
     });
 }
 
+function graficoDesempenho(req, res){
+    var idBebida = req.body.idBebida;
+
+    medidaModel.graficoDesempenho(idBebida).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     buscarUltimasMedidas,
+    graficoDesempenho,
     totalSaidas,
     buscarMedidasEmTempoReal
 
