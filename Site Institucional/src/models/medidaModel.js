88360e124ec0094c96a8a6c2696ff2b0fa584781
Melissa Neves <melissa.neves@sptech.school>
2023-05-19 11:22:39 -0300

    @melissaSBneves sessão métricas da dashboar conectada ao banco de dados

--- Site Institucional/src/models/medidaModel.js
+++ Site Institucional/src/models/medidaModel.js
@@ -62,9 +62,25 @@ function buscarMedidasEmTempoReal(idAquario) {
 }
 
 function totalSaidas(idBebida){
+    // instrucaoSql = `
+    // SELECT count(aprox_registro) AS "totalSaidas" FROM tb_registro JOIN tb_sensor ON id_sensor = fk_sensor 
+	// JOIN tb_dispenser ON id_dispenser = fk_dispenser WHERE aprox_registro = 1 AND fk_bebida = ${idBebida};
+    // `
+
     instrucaoSql = `
-    SELECT count(aprox_registro) AS "totalSaidas" FROM tb_registro JOIN tb_sensor ON id_sensor = fk_sensor 
-	JOIN tb_dispenser ON id_dispenser = fk_dispenser WHERE aprox_registro = 1 AND fk_bebida = ${idBebida};
+    SELECT nome_bebida AS "nomeBebidaBD", 
+        (SELECT count(aprox_registro) AS "totalSaidas" FROM tb_registro JOIN tb_sensor ON id_sensor = fk_sensor 
+	JOIN tb_dispenser ON id_dispenser = fk_dispenser WHERE aprox_registro = 1 AND fk_bebida = ${idBebida}) AS "totalSaidasBD",
+		(select count(id_bebida) FROM tb_bebida WHERE id_bebida =  ${idBebida}) AS "totalUnidadesBD",
+		timestampdiff(week, prazo_inicio, prazo_final) AS "tempoTesteBD",
+        meta_geral AS "metaGeralBD"
+        FROM tb_registro 
+            JOIN tb_sensor ON id_sensor = fk_sensor 
+		    JOIN tb_dispenser ON id_dispenser = fk_dispenser 
+            JOIN tb_maquina ON id_maquina = fk_maquina 
+            JOIN tb_bebida ON fk_bebida = id_bebida 
+                WHERE id_bebida =  ${idBebida}
+                limit 1;
     `
 
     return database.executar(instrucaoSql);
