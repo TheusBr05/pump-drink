d9f5033a7b9887183a461a749aa5054c48f4c485
MatheusHenrique005 <matheus.rsantos@sptech.school>
2023-05-31 12:13:17 -0300

    @MatheusHenrique005 select da tela de unidades

--- Banco_de_Dados/selects_unidade.sql
+++ Banco_de_Dados/selects_unidade.sql
@@ -1,11 +1,17 @@
-SELECT tb_local.nome, concat(rua, ', ', ifnull(numero, 'S/N'), ' - ', bairro, ' - ', cep, ' - ', cidade, ' - ', estado) as 'endereco',
-	count(id_registro) as 'saidas'
+SELECT tb_local.nome,
+    concat(
+        rua, ', ',
+        ifnull(numero, 'S/N'), ' - ',
+        bairro, ' - ',
+        cep, ' - ',
+        cidade, ' - ',
+        estado) as 'endereco',
+    count(id_registro) as 'saidas'
 FROM tb_maquina
     JOIN tb_local ON tb_local.id_local = tb_maquina.fk_local
     JOIN tb_dispenser ON tb_dispenser.fk_maquina = tb_maquina.id_maquina
     JOIN tb_bebida ON tb_bebida.id_bebida = tb_dispenser.fk_bebida
     JOIN tb_sensor ON tb_dispenser.id_dispenser = tb_sensor.fk_dispenser
     JOIN tb_registro ON tb_sensor.id_sensor = tb_registro.fk_sensor
-    WHERE id_bebida = 5
-    GROUP BY fk_local
-;
+WHERE id_bebida = 5
+GROUP BY fk_local;
