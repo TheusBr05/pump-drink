7d8514337a23e69c7cef605eb0ae636d0171cdf2
Naufel2510 <felipe.santiago@sptech.school>
2023-05-31 12:19:31 -0300

    @Naufel2510 ajuste

--- Site_Institucional/src/routes/unidade.js
+++ Site_Institucional/src/routes/unidade.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
+var unidadeController = require("../controllers/unidadeController");
 
 router.get("/unidade/:bebida", function (req, res) {
     unidadeController.alerta_unidade(req, res);
